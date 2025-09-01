5bf679fe7e3f8f7d6153cd79442d3ff43a0c7028
Killian GOMES <killian.gomes@soprasteria.com>
2025-09-01 11:38:57 +0200

    Simulation 01 - Get Products

---
+++ gatling-demo/src/test/java/com.gatlinglab/S01_GetProductsSimulation.java
@@ -0,0 +1,26 @@
+package com.gatlinglab;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+public class S01_GetProductsSimulation extends Simulation {
+
+    HttpProtocolBuilder httpProtocol = http
+            .baseUrl("http://localhost:8080")
+            .acceptHeader("application/json");
+
+    ScenarioBuilder scn = scenario("Get products")
+            .exec(
+                    http("Get products")
+                            .get("/products")
+            );
+
+    {
+        setUp(
+                scn.injectOpen(atOnceUsers(1))
+        ).protocols(httpProtocol);
+    }
+}
